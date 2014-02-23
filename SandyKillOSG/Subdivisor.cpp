1704c7ce72c9ffa965f6329f29c054710e206e52
Janisse <j_clement_42@live.fr>
2014-02-23 15:53:29 +0100

    - Essaie de subdivision (Beaucoup de bug a resourdre, mais je m'en charge)

--- SandyKillOSG/Subdivisor.cpp
+++ SandyKillOSG/Subdivisor.cpp
@@ -4,6 +4,58 @@
 
 void subdivide(ref_ptr<Node110> node110)
 {
+	//Init
+	ref_ptr<DrawElementsUInt> face = new DrawElementsUInt(PrimitiveSet::POINTS);
+
+	//Vide les faces de la geometry
+	node110->getGeometry()->getPrimitiveSetList().clear();
+	ref_ptr<Vec3Array> newFaces = new Vec3Array;
+
+	//On parcourt toutes les faces
+	for(int i=0; i<node110->getFaces()->size(); i++)
+	{
+		//On parcourt les 3 points de la face
+		Vec3 pt1 = node110->getVertexs()->at(node110->getFaces()->at(i).x());
+		Vec3 pt2 = node110->getVertexs()->at(node110->getFaces()->at(i).y());
+		Vec3 pt3 = node110->getVertexs()->at(node110->getFaces()->at(i).z());
+
+		//On crée les 3 nouveaux points
+		Vec3 pt4 = middlePoint(pt1, pt2);
+		Vec3 pt5 = middlePoint(pt1, pt3);
+		Vec3 pt6 = middlePoint(pt3, pt2);
+
+		//On ajoute les nouveaux points a la geometry
+		node110->getVertexs()->push_back(pt4);
+		node110->getVertexs()->push_back(pt5);
+		node110->getVertexs()->push_back(pt6);
+
+		//On creer les 4 nouvelles faces
+		//Face 1 (pt1, pt4, pt6)
+		face->push_back(node110->getFaces()->at(i).x());
+		face->push_back(node110->getVertexs()->size()-3);
+		face->push_back(node110->getVertexs()->size()-1);
+
+		//Face 2 (pt4, pt2, pt5)
+		face->push_back(node110->getVertexs()->size()-3);
+		face->push_back(node110->getFaces()->at(i).y());
+		face->push_back(node110->getVertexs()->size()-2);
+
+		//Face 3 (pt4, pt5, pt6)
+		face->push_back(node110->getVertexs()->size()-3);
+		face->push_back(node110->getVertexs()->size()-2);
+		face->push_back(node110->getVertexs()->size()-1);
+
+		//Face 4 (pt6, pt5, pt3)
+		face->push_back(node110->getVertexs()->size()-1);
+		face->push_back(node110->getVertexs()->size()-2);
+		face->push_back(node110->getFaces()->at(i).z());
+	}
+
+	//On ajoute les faces mises a jour a la geometry
+	node110->getGeometry()->addPrimitiveSet(face);
+
+
+	/*
 	ref_ptr<Geometry> node110Geometry = node110->getGeometry();
 	ref_ptr<Geode> node110Geode = node110->getGde();
 	ref_ptr<Vec3Array> vertexArray = (Vec3Array*)node110Geometry->getVertexArray();
@@ -62,7 +114,7 @@ void subdivide(ref_ptr<Node110> node110)
 		node110Geometry->addPrimitiveSet(face3);
 		node110Geometry->addPrimitiveSet(face4);
 
-	}
+	}*/
 }
 
 Vec3f middlePoint(Vec3f pt1, Vec3f pt2)
